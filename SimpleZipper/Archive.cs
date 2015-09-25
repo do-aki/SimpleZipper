93460533a6112d5b2519364dc76ec049f556b0cb
do_aki <do.hiroaki@gmail.com>
2015-09-25 15:44:32 +0900

    imprement

---
+++ SimpleZipper/Archive.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Ionic.Zip;
+
+namespace SimpleZipper
+{
+    class Archive
+    {
+        public static void ZipDirectory(string dir)
+        {
+            string zipfile_name = dir + ".zip";
+            if (File.Exists(zipfile_name))
+            {
+                return;
+            }
+
+            using (var zip = new ZipFile(Encoding.GetEncoding("shift_jis")))
+            {
+                zip.CompressionLevel = Ionic.Zlib.CompressionLevel.BestCompression;
+
+                var di = new DirectoryInfo(dir);
+
+                foreach (var i in di.EnumerateFileSystemInfos())
+                {
+                    if (FileAttributes.Directory == (i.Attributes & FileAttributes.Directory))
+                    {
+                        zip.AddDirectory(i.FullName, i.Name);
+                    }
+                    else if (FileAttributes.Hidden != (i.Attributes & FileAttributes.Hidden))
+                    {
+                        zip.AddFile(i.FullName, "");
+                    }
+                }
+
+                zip.Save(zipfile_name);
+            }
+        }
+
+        public static string[] extractDirectory(string[] files)
+        {
+            List<string> dirs = new List<string>();
+            foreach (var f in files)
+            {
+                if (Directory.Exists(f))
+                {
+                    dirs.Add(f);
+                }
+            }
+
+            return dirs.ToArray();
+        }
+    }
+}
--- SimpleZipper/MainWindow.xaml.cs
+++ SimpleZipper/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SimpleZipper
 {
@@ -23,6 +16,46 @@ namespace SimpleZipper
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void changeStatus(int parcent, string status)
+        {
+            progressStatus.Value = parcent;
+            textStatus.Text = status;
+        }
+
+        private async Task archiveDirectories(string[] dirs)
+        {
+            int complete = 0;
+
+            foreach (var f in dirs)
+            {
+                int parcent = (int)(100 * ((float)complete++ / dirs.Length));
+                changeStatus(parcent, "Compressing " + Path.GetFileName(f));
+                await Task.Run(() => Archive.ZipDirectory(f));
+            }
+        }
+
+        private void Window_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.All;
+            }
+        }
+
+        private async void Window_Drop(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                return;
+            }
+
+            var dirs = Archive.extractDirectory((string[])e.Data.GetData(DataFormats.FileDrop));
+
+            changeStatus(0, "Busy");
+            await this.archiveDirectories(dirs);
+            changeStatus(100, "Waiting...");
         }
     }
 }
